76e8c7e687f99181b0f749bdc73361f1fc0d7be1
Chi Hwan Kim <rlaclghks123@naver.com>
2022-07-18 16:30:58 +0900

    first time nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import * as React from 'react';
-import { Reset } from 'styled-reset';
+import * as React from "react";
+import styled from "styled-components";
+import { Reset } from "styled-reset";
+
+const Nav = styled.div`
+  background-color: teal;
+  width: 400px;
+  margin-top: 300px;
+`;
 
 function App() {
   return (
     <>
       <Reset />
-      <div className='App'>
+      <Nav>Hi</Nav>
+      <div className="App">
         <h1>새로운 레퍼지토리</h1>
       </div>
     </>
