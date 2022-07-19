46a960712009b582debc1b4e78587313d5b40af7
Chi Hwan Kim <rlaclghks123@naver.com>
2022-07-19 14:49:14 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import * as React from 'react';
-import { Reset } from 'styled-reset';
-import styled from 'styled-components';
+import * as React from "react";
+import { Reset } from "styled-reset";
+import styled from "styled-components";
+
+const Nav = styled.div`
+  margin-top: 300px;
+  background-color: teal;
+`;
 
 const NavBar = styled.div`
   height: 300px;
   background-color: violet;
+  position: absolute;
+  top: 0px;
 `;
 
 function App() {
   return (
     <>
       <Reset />
-      <NavBar>네브바입니다</NavBar>
+      <NavBar>네브바입니다.</NavBar>
+      <Nav>Nav</Nav>
     </>
   );
 }
